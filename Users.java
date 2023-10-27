295ca87efd2695bea0aa75138705386b3b8645c5
kmnewma4 <91095865+kmnewma4@users.noreply.github.com>
2023-10-27 16:30:01 -0700

    Add files via upload

---
+++ Users.java
@@ -0,0 +1,27 @@
+package loginJavaFX;
+public class Users {
+	private String name; 
+	private String email;
+	private String username;
+	private String password;
+	
+	public Users(String name, String email, String username, String password) {
+		this.name = name;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
